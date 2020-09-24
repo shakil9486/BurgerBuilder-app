7b2e17b223b876b606f820c208123a6b0f6835b9
shakil9486 <shakil9486@gmail.com>
2020-09-24 17:30:47 +0600

    128. Starting the Burger Component 8

--- BurgerIngredient.js
+++ BurgerIngredient.js
@@ -1,6 +1,8 @@
 import React, { Component} from 'react';
 import PropTypes from 'prop-types';
 
+import classes from './BurgerIngredient.css';
+
 class BurgerIngredient extends Component {
     render () {
         let ingredient = null;
